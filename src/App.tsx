82535d265019d2f25b40670f141f90fd08c747c7
AuspiciousAspirant <162094590+AuspiciousAspirant@users.noreply.github.com>
2025-08-29 21:46:11 -0400

    Router Set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
-import "./App.css"
+import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
+import About from "./components/About";
+import Contact from "./components/Contact";
+//import Footer from "./components/";
+import Landing_Page from "./components/Landing_Page";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
 
   return (
-    <> 
-    <h1>Hello World</h1>
+    <>
+    <BrowserRouter>
+    <Header>
+        <Routes>
+          <Route path='/About' element={<About/>} />
+          <Route path='/Contact' element={<Contact/>} />
+          <Route path='/Landing_Page' element={<Landing_Page/>} />
+          <Route path='/Projects' element={<Projects/>} />
+          <Route path='/Skills' element={<Skills/>} />
+        </Routes>
+    </Header>
+
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Landing_Page.tsx
@@ -0,0 +1,9 @@
+//This should be a Modal
+
+export default function Landing_Page() {
+    return (
+        <div>
+            <h1>Landing Page</h1>
+        </div>
+    )
+}
